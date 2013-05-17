58ceaf815662028ea447c21e42484a03c3a402f3
silvansky <v.silvansky@gmail.com>
2013-05-17 12:40:12 +0400

    convert_video_4_ios.sh improved: video bitrate and title from filename

--- convert_video_4_ios.sh
+++ convert_video_4_ios.sh
@@ -4,5 +4,5 @@ FILES=$@
 
 for F in $FILES; do
 	BASENAME="${F%.*}"
-	ffmpeg -i $F -acodec libfaac -ab 128k -vcodec mpeg4 -b 1200k -mbd 2 -ac 2 -cmp 2 -subcmp 2 $BASENAME.mp4
+	ffmpeg -i $F -acodec libfaac -ab 128k -vcodec mpeg4 -b:v 1200k -mbd 2 -ac 2 -cmp 2 -subcmp 2 -metadata title="$BASENAME" $BASENAME.mp4
 done;

642ebd806cd099f5e4fff1f7aef04dfa4fd4854e
silvansky <v.silvansky@gmail.com>
2013-09-30 18:28:35 +0400

    devlifestat.sh added

--- installable.sh
+++ installable.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-SCRIPTS="openrandom.sh toggle_finder_show_hidden.sh gpull.sh gpush.sh gpullcb.sh gpushcb.sh gitcb.sh habrastat.sh"
+SCRIPTS="openrandom.sh toggle_finder_show_hidden.sh gpull.sh gpush.sh gpullcb.sh gpushcb.sh gitcb.sh habrastat.sh devlifestat.sh"
 DEST=/usr/local/bin

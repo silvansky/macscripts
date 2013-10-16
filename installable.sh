011fa093885908c8732291f6b03068fe47df4c14
silvansky <v.silvansky@gmail.com>
2013-10-16 15:08:18 +0400

    Togif added to installable

--- installable.sh
+++ installable.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-SCRIPTS="openrandom.sh toggle_finder_show_hidden.sh gpull.sh gpush.sh gpullcb.sh gpushcb.sh gitcb.sh habrastat.sh devlifestat.sh"
+SCRIPTS="openrandom.sh toggle_finder_show_hidden.sh gpull.sh gpush.sh gpullcb.sh gpushcb.sh gitcb.sh habrastat.sh devlifestat.sh togif.sh"
 DEST=/usr/local/bin
